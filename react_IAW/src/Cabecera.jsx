7faffbbb60d84cbf9ace539c344d7e2b699cc9a1
AdrianRai <adriancamposvargas4@gmail.com>
2024-10-15 14:28:06 +0200

    20241015-IAW

--- react_IAW/src/Cabecera.jsx
+++ react_IAW/src/Cabecera.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
-export default function Cabecera() {
+export function Cabecera1() {
   return (
-    <h1>Cabecera</h1>
+    <h1>Clase de IAW 2024</h1>
+  )
+}
+
+export function Cabecera2() {
+  return (
+    <h2>Segunda Cabecera</h2>
+  )
+}
+
+export function Cabecera3(props) {
+  return (
+    <>
+      <li>{props.mensaje}</li>
+      <li>{props.otro}</li>
+    </>
+  )
+}
+
+export function Cabecera4({mensaje="mensaje123",otro}) {
+  return (
+    <>
+      <li>{mensaje}</li>
+      <li>{otro}</li>
+    </>
   )
 }
--- react_IAW/src/Web.jsx
+++ react_IAW/src/Web.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import './web.css'
+import {Cabecera1, Cabecera2, Cabecera3, Cabecera4 } from './Cabecera'
 
 function Web() {
     return(
-        <div>Adrian Rai Campos Vargas</div>
+        <div>
+            <div>Adrian Rai Campos Vargas</div>
+            <Cabecera1/>
+            <Cabecera1/>
+            <Cabecera2/>
+            <ol>
+                <Cabecera3 mensaje="Es REACT" otro="otro texto"/>
+                <Cabecera4 mensaje="Es REACT2" otro="otro texto"/>
+                <Cabecera4 otro="otro texto"/>
+            </ol>
+        </div>
     )
 }
 
