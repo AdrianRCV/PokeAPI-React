a5b6c46762805e00f919de5b15b4bd19d3b87531
AdrianRai <adriancamposvargas4@gmail.com>
2024-11-20 13:19:01 +0100

    20241120

--- ReactIAW/src/paginaWeb/Web.jsx
+++ ReactIAW/src/paginaWeb/Web.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import Contenido from './Contenido'
-import Footer from './Footer'
 
 function Web() {
   return (
     <>
     <Header/>
     <Contenido/>
-    <Footer RRSS={[{nombre:"Facebook",url:"hola"},{nombre:"Instagram",url:"adios"},{nombre:"Linkedin",url:"otra"},{nombre:"Google",url:"google"}]}/>
     </>
   )
 }
