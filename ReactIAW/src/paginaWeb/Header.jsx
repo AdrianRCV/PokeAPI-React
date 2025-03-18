b19c8848a5e834445cb43fedba035cd36a605bf2
AdrianRCV <adriancamposvargas4@gmail.com>
2025-03-18 10:21:58 +0100

    pruebajenkins2

--- ReactIAW/src/paginaWeb/Header.jsx
+++ ReactIAW/src/paginaWeb/Header.jsx
@@ -4,7 +4,7 @@ import style from './header.module.css'
 function Header() {
   return (
     <header className={style.cabecera}>
-        <h1>USO BÁSICO RUTAS Y HOOKS CON POKEAPI y probando jenkins</h1>
+        <h1>USO BÁSICO RUTAS Y HOOKS CON POKEAPI y probando jenkins2</h1>
     </header>
   )
 }
